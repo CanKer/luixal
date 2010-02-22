4ac3268039264c24fc0f070902362410ece31393
luixal <luixal@5bd6febc-1a5d-11df-aa99-b37d35d76e06>
2010-02-22 13:50:27 +0000

    Reducing things... Simple AutomataFD wrap (working fine :D) AutomataFN class renamed as AutomataFND.
    Next to do:
    	- Change Automata class (just the basic non-graph-related stuff.
    	- Refactor and Complete AutomataFD class.
    	- Implement in a similar way AutomataFND class.

--- PL21/src/pl21/Automata/Automata.java
+++ PL21/src/pl21/Automata/Automata.java
@@ -5,7 +5,7 @@
 
 package pl21.Automata;
 
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -14,7 +14,128 @@ import java.util.HashSet;
  */
 public abstract class Automata {
 
-    protected String id;              // Automata's IDentificator
-    protected HashSet states;         // state's container
-    protected HashMap transitions;    // transition's container (key = origin state, value = transition/s, depending on Automata's condition)
+    protected String id;                // Automata's IDentificator
+    protected HashSet<String> states;           // state's container
+    protected HashSet<String> alphabet;         // automata's alphabet
+    protected String init_state;        // init state
+    protected String final_state;       // final state
+
+    Automata(String id, HashSet states, HashSet alphabet, String init_state, String final_state) {
+        this.id = id;
+        this.states = states;
+        this.alphabet = alphabet;
+        this.init_state = init_state;
+        this.final_state = final_state;
+    }
+
+    Automata(String id) {
+        this.id = id;
+        this.states = new HashSet<String>();
+        this.alphabet = new HashSet<String>();
+        this.init_state = "";
+        this.final_state = "";
+    }
+
+    Automata() {
+        this.id = "Generic Automata";
+        this.states = new HashSet<String>();
+        this.alphabet = new HashSet<String>();
+        this.init_state = "";
+        this.final_state = "";
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    // States related methods:
+    public boolean addState(String state) {
+        return this.states.add(state);
+    }
+
+    public boolean addInitState(String state) {
+        if (this.addState(state)) {
+            this.setInitState(state);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean addFinalState(String state) {
+        if (this.addState(state)) {
+            this.setFinalState(state);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    abstract boolean removeState(String state);
+
+    public Integer numberOfStates() {
+        return this.states.size();
+    }
+
+    public boolean clearStates() {
+        this.states.clear();
+        this.init_state = "";
+        this.final_state = "";
+        return (this.states.size() == 0);
+    }
+
+    public boolean isState(String state) {
+        return this.states.contains(state);
+    }
+
+    public boolean isInitState(String state) {
+        return (this.isState(state) && this.init_state.equals(state));
+    }
+
+    public boolean isFinalState(String state) {
+        return (this.isState(state) && this.final_state.equals(state));
+    }
+
+    public boolean setInitState(String state) {
+        if (this.isState(state)) {
+            this.init_state = state;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean setFinalState(String state) {
+        if (this.isState(state)) {
+            this.final_state = state;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Alphabet related methods:
+
+    public boolean addSymbol(String symbol) {
+        return this.alphabet.add(symbol);
+    }
+
+    public boolean removeSymbol(String symbol) {
+        return this.alphabet.remove(symbol);
+    }
+
+    public boolean isSymbol(String symbol) {
+        return this.alphabet.contains(symbol);
+    }
+
+    public boolean clearAlphabet() {
+        this.alphabet.clear();
+        return (this.alphabet.size() == 0);
+    }
+
+    // Transitions related methods:
+    abstract boolean addTransition(String origin_state, String dest_state, String symbol);
+    abstract boolean removeTransition(String origin_state, String dest_state, String symbol);
+    abstract ArrayList<Transition> getTransitionsFromState(String state);
+    abstract ArrayList<Transition> getTransitionsToState(String state);
 }
--- PL21/src/pl21/Automata/AutomataFD.java
+++ PL21/src/pl21/Automata/AutomataFD.java
@@ -5,10 +5,87 @@
 
 package pl21.Automata;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  *
  * @author nemesis
  */
+
+// only one transition for echar pair (state, symbol)
 public class AutomataFD {
+
+    private HashMap<String, HashMap<String, String>> transitions;
+
+    AutomataFD() {
+        this.transitions = new HashMap<String, HashMap<String, String>>();
+    }
+
+    public boolean addState(String state) {
+        if (this.transitions.containsKey(state)) {
+            return false;
+        } else {
+            this.transitions.put(state, new HashMap<String, String>());
+            return true;
+        }
+    }
+
+    public boolean addTransition(String orig_state, String dest_state, String symbol) {
+        if (this.transitions.containsKey(orig_state) && this.transitions.containsKey(dest_state)) {
+            this.transitions.get(orig_state).put(symbol, dest_state);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String toString() {
+        String aux = "Generic AutomataFD\n";
+        aux += "States: " + this.transitions.keySet().toString() + "\n";
+        for (String state:this.transitions.keySet()) {
+            aux += "State: " + state + ":\n";
+            for (String symbol:this.transitions.get(state).keySet()) {
+                aux += "\t" + symbol + " --> " + this.transitions.get(state).get(symbol) + "\n";
+            }
+        }
+        aux += "\n\nEnd of Generic AutomataFD.";
+        return aux;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Running AutomataFD.Main...\n");
+        System.out.print("Creating a generic AutomataFD... ");
+        AutomataFD afd = new AutomataFD();
+        System.out.println("DONE!");
+        System.out.println("Adding States:");
+        System.out.print("\tState 'e1'... ");
+        afd.addState("e1");
+        System.out.println("DONE!");
+        System.out.print("\tState 'e2'... ");
+        afd.addState("e2");
+        System.out.println("DONE!");
+        System.out.print("\tState 'e3'... ");
+        afd.addState("e3");
+        System.out.println("DONE!");
+        System.out.print("\tState 'e4'... ");
+        afd.addState("e4");
+        System.out.println("DONE!");
+        System.out.println("Adding transitions:");
+        System.out.print("Transition from 'e1' to 'e2' through symbol 'a'... ");
+        afd.addTransition("e1", "e2", "a");
+        System.out.println("DONE!");
+        System.out.print("Transition from 'e2' to 'e3' through symbol 'b'... ");
+        afd.addTransition("e2", "e3", "b");
+        System.out.println("DONE!");
+        System.out.print("Transition from 'e2' to 'e4' through symbol 'a'... ");
+        afd.addTransition("e2", "e4", "a");
+        System.out.println("DONE!");
+        System.out.print("Transition from 'e4' to 'e3' through symbol 'b'... ");
+        afd.addTransition("e4", "e3", "b");
+        System.out.println("DONE!");
+        System.out.println("\n\nNow showing the automata (toString test :P):");
+        System.out.println(afd);
+    }
 
 }
