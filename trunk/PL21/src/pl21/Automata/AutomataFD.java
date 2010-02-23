f423971b9a45178fc63a4377e1b66366c4be91c1
luixal <luixal@5bd6febc-1a5d-11df-aa99-b37d35d76e06>
2010-02-23 18:11:35 +0000

    + Automata class changed for being a simple abstract class (almost no implementation). + AutomataFD refactored, inherited methods added, implemented simple ones.
    Next to do:
    	- Implement methods in AutomataFD (removeState, getAlphabet, removeTransition).
    	- Implement AutomataFND class.

--- trunk/PL21/src/pl21/Automata/AutomataFD.java
+++ trunk/PL21/src/pl21/Automata/AutomataFD.java
@@ -5,8 +5,8 @@
 
 package pl21.Automata;
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
 /**
  *
@@ -14,39 +14,116 @@ import java.util.HashMap;
  */
 
 // only one transition for echar pair (state, symbol)
-public class AutomataFD {
+public class AutomataFD extends Automata {
 
-    private HashMap<String, HashMap<String, String>> transitions;
+    private HashMap<String, HashMap<String, String>> graph;
 
     AutomataFD() {
-        this.transitions = new HashMap<String, HashMap<String, String>>();
+        super();
+        this.id += this.id + " - AFD";
+        this.graph = new HashMap<String, HashMap<String, String>>();
+    }
+
+    AutomataFD(String id) {
+        super(id);
+        this.graph = new HashMap<String, HashMap<String, String>>();
+    }
+
+    AutomataFD(AutomataFD afd) {
+        super(afd);
+        this.graph = new HashMap<String, HashMap<String, String>>(afd.graph);
     }
 
     public boolean addState(String state) {
-        if (this.transitions.containsKey(state)) {
+        if (this.graph.containsKey(state)) {
             return false;
         } else {
-            this.transitions.put(state, new HashMap<String, String>());
+            this.graph.put(state, new HashMap<String, String>());
             return true;
         }
     }
 
     public boolean addTransition(String orig_state, String dest_state, String symbol) {
-        if (this.transitions.containsKey(orig_state) && this.transitions.containsKey(dest_state)) {
-            this.transitions.get(orig_state).put(symbol, dest_state);
+        if (this.graph.containsKey(orig_state) && this.graph.containsKey(dest_state)) {
+            this.graph.get(orig_state).put(symbol, dest_state);
             return true;
         } else {
             return false;
         }
     }
 
+    @Override
+    boolean clearAll() {
+        this.graph.clear();
+        return (this.graph.size() == 0);
+    }
+
+    @Override
+    boolean isState(String state) {
+        return this.graph.containsKey(state);
+    }
+
+    @Override
+    boolean setInitState(String state) {
+        if (this.graph.containsKey(state)) {
+            this.init_state = state;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    boolean isInitState(String state) {
+        return this.init_state.equals(state);
+    }
+
+    @Override
+    boolean setFinalState(String state) {
+        if (this.graph.containsKey(state)) {
+            this.final_state = state;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    boolean isFinalState(String state) {
+        return this.final_state.equals(state);
+    }
+
+    @Override
+    boolean removeState(String state) {
+        throw new UnsupportedOperationException("Not supported yet.");
+        // removing 'state' from graph's keys
+        // removing transitions in which 'state' appears as destination state
+    }
+
+    @Override
+    Integer getNumberOfStates() {
+        return this.graph.size();
+    }
+
+    @Override
+    Set<String> getAlphabet() {
+        throw new UnsupportedOperationException("Not supported yet.");
+        // mantaining a set variable for alphabet??
+        // going through every transition populating the returning set??
+    }
+
+    @Override
+    boolean removeTransition(String origin_state, String dest_state, String symbol) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
     public String toString() {
-        String aux = "Generic AutomataFD\n";
-        aux += "States: " + this.transitions.keySet().toString() + "\n";
-        for (String state:this.transitions.keySet()) {
+        String aux = this.id + "\n";
+        aux += "States: " + this.graph.keySet().toString() + "\n";
+        for (String state:this.graph.keySet()) {
             aux += "State: " + state + ":\n";
-            for (String symbol:this.transitions.get(state).keySet()) {
-                aux += "\t" + symbol + " --> " + this.transitions.get(state).get(symbol) + "\n";
+            for (String symbol:this.graph.get(state).keySet()) {
+                aux += "\t" + symbol + " --> " + this.graph.get(state).get(symbol) + "\n";
             }
         }
         aux += "\n\nEnd of Generic AutomataFD.";
@@ -87,5 +164,4 @@ public class AutomataFD {
         System.out.println("\n\nNow showing the automata (toString test :P):");
         System.out.println(afd);
     }
-
 }
