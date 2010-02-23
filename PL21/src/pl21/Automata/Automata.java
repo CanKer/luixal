1e3f159066a0c196b31efe289b8831a1308c4bd1
luixal <luixal@5bd6febc-1a5d-11df-aa99-b37d35d76e06>
2010-02-23 18:11:35 +0000

    + Automata class changed for being a simple abstract class (almost no implementation). + AutomataFD refactored, inherited methods added, implemented simple ones.
    Next to do:
    	- Implement methods in AutomataFD (removeState, getAlphabet, removeTransition).
    	- Implement AutomataFND class.

--- PL21/src/pl21/Automata/Automata.java
+++ PL21/src/pl21/Automata/Automata.java
@@ -5,8 +5,7 @@
 
 package pl21.Automata;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -14,128 +13,63 @@ import java.util.HashSet;
  */
 public abstract class Automata {
 
-    protected String id;                // Automata's IDentificator
-    protected HashSet<String> states;           // state's container
-    protected HashSet<String> alphabet;         // automata's alphabet
-    protected String init_state;        // init state
-    protected String final_state;       // final state
-
-    Automata(String id, HashSet states, HashSet alphabet, String init_state, String final_state) {
-        this.id = id;
-        this.states = states;
-        this.alphabet = alphabet;
-        this.init_state = init_state;
-        this.final_state = final_state;
-    }
+    protected String id;                    // Automata's IDentificator
+//    protected HashSet<String> states;     // state's container
+//    protected HashSet<String> alphabet;   // automata's alphabet
+    protected String init_state;            // init state
+    protected String final_state;           // final state
 
     Automata(String id) {
         this.id = id;
-        this.states = new HashSet<String>();
-        this.alphabet = new HashSet<String>();
-        this.init_state = "";
-        this.final_state = "";
     }
 
     Automata() {
         this.id = "Generic Automata";
-        this.states = new HashSet<String>();
-        this.alphabet = new HashSet<String>();
         this.init_state = "";
         this.final_state = "";
+    }
+
+    Automata(Automata a) {
+        this.id = a.getId();
+        this.init_state = a.getInitState();
+        this.final_state = a.getFinalState();
     }
 
     public void setId(String id) {
         this.id = id;
     }
 
+    public String getId() {
+        return this.id;
+    }
+
+    public String getInitState() {
+        return this.init_state;
+    }
+
+    public String getFinalState() {
+        return this.final_state;
+    }
+
+    // not-for-monkeys methods:
+    abstract boolean clearAll();
+    
     // States related methods:
-    public boolean addState(String state) {
-        return this.states.add(state);
-    }
-
-    public boolean addInitState(String state) {
-        if (this.addState(state)) {
-            this.setInitState(state);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean addFinalState(String state) {
-        if (this.addState(state)) {
-            this.setFinalState(state);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
+    abstract boolean addState(String state);
+    abstract boolean isState(String state);
+    abstract boolean setInitState(String state);
+    abstract boolean isInitState(String state);
+    abstract boolean setFinalState(String state);
+    abstract boolean isFinalState(String state);
     abstract boolean removeState(String state);
-
-    public Integer numberOfStates() {
-        return this.states.size();
-    }
-
-    public boolean clearStates() {
-        this.states.clear();
-        this.init_state = "";
-        this.final_state = "";
-        return (this.states.size() == 0);
-    }
-
-    public boolean isState(String state) {
-        return this.states.contains(state);
-    }
-
-    public boolean isInitState(String state) {
-        return (this.isState(state) && this.init_state.equals(state));
-    }
-
-    public boolean isFinalState(String state) {
-        return (this.isState(state) && this.final_state.equals(state));
-    }
-
-    public boolean setInitState(String state) {
-        if (this.isState(state)) {
-            this.init_state = state;
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean setFinalState(String state) {
-        if (this.isState(state)) {
-            this.final_state = state;
-            return true;
-        } else {
-            return false;
-        }
-    }
+    abstract Integer getNumberOfStates();
 
     // Alphabet related methods:
-
-    public boolean addSymbol(String symbol) {
-        return this.alphabet.add(symbol);
-    }
-
-    public boolean removeSymbol(String symbol) {
-        return this.alphabet.remove(symbol);
-    }
-
-    public boolean isSymbol(String symbol) {
-        return this.alphabet.contains(symbol);
-    }
-
-    public boolean clearAlphabet() {
-        this.alphabet.clear();
-        return (this.alphabet.size() == 0);
-    }
+    abstract Set<String> getAlphabet();
 
     // Transitions related methods:
     abstract boolean addTransition(String origin_state, String dest_state, String symbol);
     abstract boolean removeTransition(String origin_state, String dest_state, String symbol);
-    abstract ArrayList<Transition> getTransitionsFromState(String state);
-    abstract ArrayList<Transition> getTransitionsToState(String state);
+//    abstract ArrayList<Transition> getTransitionsFromState(String state);
+//    abstract ArrayList<Transition> getTransitionsToState(String state);
 }
